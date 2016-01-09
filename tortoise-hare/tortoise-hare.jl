7c06d74ea9d6d1de6c9bf23d1bc2a3d99a6e04f5
George Lesica <george@lesica.com>
2016-01-09 15:21:44 -0700

    Different mutation strategy.

--- tortoise-hare/tortoise-hare.jl
+++ tortoise-hare/tortoise-hare.jl
@@ -6,7 +6,11 @@ function run(n, k, popsize)
   pop = rand(NK.Population, ls, popsize)
   println("G = ", 1, ", N = ", n, ", K = ", k, ", F = ", NK.popfits(pop, ls) |> mean)
   for gen = 2:1000
-    NK.propsel!(pop, ls)
+    fits = NK.popfits(pop, ls)
+    imax = indmax(fits)
+    for i = 1:NK.popsize(pop)
+      pop[:,i] = pop[:,imax]
+    end
     NK.bsmutate!(pop, ls)
   end
   println("G = ", 1000, ", N = ", n, ", K = ", k, ", F = ", NK.popfits(pop, ls) |> mean)

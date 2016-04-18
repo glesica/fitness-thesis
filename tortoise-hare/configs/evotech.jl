07d8c4c1bab542df2b138ea5e7d3c74e6d753ffd
George Lesica <george@lesica.com>
2016-04-17 23:45:45 -0600

    Update evotech config.

--- tortoise-hare/configs/evotech.jl
+++ tortoise-hare/configs/evotech.jl
@@ -12,6 +12,7 @@ const N_VALUE = 15
 const K_VALUES = [0, 3, 6, 9, 12]
 const P_VALUES = [0.1, 0.3, 0.5, 0.7, 0.9]
 const POP_SIZE = 1000
+const POP_COUNTS = [1, 5, 10]
 const GENS_PER_TRIAL = 1000
 const MORAN_ITERS = [200, 400, 800, 1000]
 const MUT_PROB = 0.1
